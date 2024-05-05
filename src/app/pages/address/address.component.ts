e6a45fc5836ebf420505e7962a9d5bb30c030d2d
Davi Fernandes <davifncosta@gmail.com>
2024-05-05 15:56:41 -0300

    partial: address table

--- src/app/pages/address/address.component.ts
+++ src/app/pages/address/address.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-address',
   standalone: true,
   imports: [],
   templateUrl: './address.component.html',
-  styleUrl: './address.component.scss'
+  styleUrl: './address.component.scss',
 })
 export class AddressComponent {
+  constructor(private modalService: NgbModal) {}
 
+  openModal() {
+    this.modalService.open({ ariaLabelledBy: 'modal-basic-title' });
+  }
 }
